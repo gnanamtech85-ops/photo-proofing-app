30c908ef1ea95f6d16109c30af9e5511ee549ab6
gnanamtech85-ops <gnanamtech85@gmail.com>
2025-12-23 09:22:21 +0530

    Initialize package.json for photoshare-pro project

---
+++ server.js
@@ -0,0 +1,17 @@
+{
+  "name": "photoshare-pro",
+  "version": "1.0.0",
+  "description": "Professional Photo Sharing Platform",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "pg": "^8.11.3",
+    "cloudinary": "^1.41.0",
+    "dotenv": "^16.3.1",
+    "cors": "^2.8.5"
+  }
+}
